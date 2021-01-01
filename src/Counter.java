f3afacbbf6620d543d47b01f3430bec61f55eeff
raphzam <raphaeljasonzamora@gmail.com>
2021-01-01 18:35:28 -0500

    created counter class

---
+++ src/Counter.java
@@ -0,0 +1,33 @@
+public class Counter {
+    private int count;
+
+    public Counter(){
+
+    }
+
+    public Counter(int initial){
+        count = initial;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void setCount(int count){
+        this.count = count;
+    }
+
+    public void increment(){
+        count++;
+    }
+
+    public void increment(int delta){
+        count += delta;
+    }
+
+    public void reset(){
+        count = 0;
+    }
+
+
+}
